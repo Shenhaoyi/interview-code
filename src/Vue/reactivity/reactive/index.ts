9b5143a67221ad32d9e08f6a9d8eb36cbd2a06b3
Shenhaoyi <shenhaoyi04@163.com>
2023-10-11 12:51:28 +0800

    修改命名

--- src/Vue/reactivity/reactive/index.ts
+++ src/Vue/reactivity/reactive/index.ts
@@ -11,24 +11,24 @@ const targetMap = new WeakMap<Object, Map<string, Set<AnyFn>>>();
 
 // 对副作用进行依赖追踪
 export function track(target: Object, key: string, effect: AnyFn) {
-  let depMap = targetMap.get(target);
-  if (!depMap) {
-    depMap = new Map<string, Set<AnyFn>>();
-    targetMap.set(target, depMap);
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    depsMap = new Map<string, Set<AnyFn>>();
+    targetMap.set(target, depsMap);
   }
-  let deps = depMap.get(key);
+  let deps = depsMap.get(key);
   if (!deps) {
     deps = new Set<AnyFn>();
-    depMap.set(key, deps);
+    depsMap.set(key, deps);
   }
   deps.add(effect);
 }
 
 // 依赖变更时调用，用来触发副作用
 export function trigger(target: Object, key: string) {
-  const depMap = targetMap.get(target);
-  if (!depMap) return;
-  const deps = depMap.get(key);
+  const depsMap = targetMap.get(target);
+  if (!depsMap) return;
+  const deps = depsMap.get(key);
   if (!deps) return;
   deps.forEach((effect) => effect());
 }
