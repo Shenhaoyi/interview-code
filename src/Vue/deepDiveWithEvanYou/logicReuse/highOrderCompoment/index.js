9b2b3dbfaa46707677f2cddeb07271401f5c893e
Shenhaoyi <shenhaoyi04@163.com>
2023-10-16 02:02:13 +0800

    高阶组件

---
+++ src/Vue/deepDiveWithEvanYou/logicReuse/highOrderCompoment/index.js
@@ -0,0 +1,40 @@
+const { createApp, h } = Vue;
+
+const MouseMixin = {
+  data() {
+    return {
+      x: 0,
+      y: 0,
+    };
+  },
+  methods: {
+    update(e) {
+      this.x = e.pageX;
+      this.y = e.pageX;
+    },
+  },
+  mounted() {
+    window.addEventListener('mousemove', this.update);
+  },
+  unMounted() {
+    window.removeEventListener('mousemove', this.update);
+  },
+};
+
+// 高阶组件
+function withMouse(inner) {
+  return {
+    ...MouseMixin,
+    render() {
+      return h(inner, { x: this.x, y: this.y });
+    },
+  };
+}
+
+const App = withMouse({
+  // 模板
+  props: ['x', 'y'],
+  template: '{{ x }} {{ y }}',
+});
+
+createApp(App).mount('#app');
