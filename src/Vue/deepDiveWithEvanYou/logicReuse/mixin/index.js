4f6905a43ed9e1bdb34d6670d3ccd79a25495583
Shenhaoyi <shenhaoyi04@163.com>
2023-10-16 01:42:49 +0800

    mixin

---
+++ src/Vue/deepDiveWithEvanYou/logicReuse/mixin/index.js
@@ -0,0 +1,27 @@
+const { createApp } = Vue;
+
+const MouseMixin = {
+  data() {
+    return {
+      x: 0,
+      y: 0,
+    };
+  },
+  methods: {
+    update(e) {
+      this.x = e.pageX;
+      this.y = e.pageX;
+    },
+  },
+  mounted() {
+    window.addEventListener('mousemove', this.update);
+  },
+  unMounted() {
+    window.removeEventListener('mousemove', this.update);
+  },
+};
+createApp({
+  // 模板
+  mixins: [MouseMixin],
+  template: '{{ x }} {{ y }}',
+}).mount('#app');
