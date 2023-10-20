79a04df23c3c5abe44d57546db5d1edae0c6d20c
Shenhaoyi <shenhaoyi04@163.com>
2023-10-20 17:27:14 +0800

    支持自定义scheduler进行任务调度

--- src/React/分时函数/优化/index.js
+++ src/React/分时函数/优化/index.js
@@ -7,12 +7,21 @@ btn.addEventListener('click', () => {
     container.appendChild(el);
   };
   const array = Array.from({ length: 10000 }, (_, i) => i);
-  performChunk(handler, array);
+  // 通过计时器进行调度：1000秒之后执行，执行500毫秒
+  // const scheduler = (task) => {
+  //   const start = new Date();
+  //   setTimeout(() => {
+  //     task(() => new Date() - start > 500);
+  //   }, 1000);
+  // };
+  // performChunk(handler, array, scheduler);
+  browserPerformChunk(handler, array);
 });
 
 function performChunk(
   handler, // 任务单元执行
   dataList,
+  scheduler, // 何时开启下一块的执行，每块执行多少，由scheduler进行调度
 ) {
   if (dataList.length === 0) return;
   let i = 0;
@@ -20,8 +29,8 @@ function performChunk(
   // 在空闲时间执行剩余任务
   function _run() {
     if (i >= length) return;
-    requestIdleCallback((IdleDeadline) => {
-      while (IdleDeadline.timeRemaining() > 0 && i < length) {
+    scheduler((idGoOn) => {
+      while (idGoOn() && i < length) {
         handler(dataList[i], i);
         i++;
       }
@@ -29,4 +38,15 @@ function performChunk(
     });
   }
   _run();
+}
+
+function browserPerformChunk(handler, dataList) {
+  const scheduler = (task) => {
+    // 空闲时间执行
+    // 运行多少取决于空闲时间有多少
+    requestIdleCallback((IdleDeadline) => {
+      task(() => IdleDeadline.timeRemaining() > 0);
+    });
+  };
+  performChunk(handler, dataList, scheduler);
 }
