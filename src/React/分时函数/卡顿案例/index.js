63de0f32e898e92bef5f9723875eb2681a3290b5
Shenhaoyi <shenhaoyi04@163.com>
2023-10-20 15:22:39 +0800

    优化数组构造

--- src/React/分时函数/卡顿案例/index.js
+++ src/React/分时函数/卡顿案例/index.js
@@ -1,6 +1,6 @@
 const btn = document.getElementById('btn');
 const container = document.getElementById('container');
-const array = new Array(10000).fill(undefined).map((_, i) => i);
+const array = Array.from({ length: 10000 }, (_, i) => i);
 btn.addEventListener('click', () => {
   array.forEach((i) => {
     const el = document.createElement('div');
