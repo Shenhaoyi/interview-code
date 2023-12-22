487693ec9b55d5995b047e7a74bda0daf2e1d880
Shenhaoyi <shenhaoyi04@163.com>
2023-12-22 14:45:12 +0800

    补充注释

--- src/JS/deepClone/index.ts
+++ src/JS/deepClone/index.ts
@@ -1,8 +1,8 @@
 const cache = new WeakMap();
 
-/* 
+/*
   深拷贝，实现要点:
-  1、非对象类型直接返回（函数也是）
+  1、非对象类型直接返回（函数也是）,注意 null 的处理
   2、对象类型判断，按类型进行处理（其他类型可以再拓展，这里只判断Array）
   3、处理循环引用-缓存
   4、把原型也继承下来
@@ -12,7 +12,7 @@ const cache = new WeakMap();
 */
 export function deepClone(value: any) {
   if (typeof value !== 'object' || value === null) {
-    /* 
+    /*
       1. typeof 函数 = "function"，会走到这里，函数不用特殊处理，直接返回就行
       2. null 特例，需要判断一下
     */
@@ -20,13 +20,13 @@ export function deepClone(value: any) {
   }
   // 有缓存，从缓存中获取
   if (cache.has(value)) return cache.get(value);
-  /* 
+  /*
     这里只考虑数组和一把对象的情况。如果要处理 Set 等其他类，可以把 new get set 等进行抽象
   */
   const result: Record<string, any> = Array.isArray(value)
     ? [] // 数组直接创建就行
-    : Object.create(Object.getPrototypeOf(value)); // 继承对象的原型上的属性和方法
-  /* 
+    : /*注意，不推荐使用 Object.setPrototypeOf*/ Object.create(Object.getPrototypeOf(value)); // 继承对象的原型上的属性和方法
+  /*
     循环引用处理：先缓存，再递归，递归过程中遇到同一个对象直接返回缓存结果就行。
   */
   cache.set(value, result);
