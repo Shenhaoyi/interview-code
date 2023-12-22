503c1392ece9fc94f79c9df38ead4263c0475879
Shenhaoyi <shenhaoyi04@163.com>
2023-12-22 14:11:57 +0800

    补充注释

--- src/JS/instanceof/index.ts
+++ src/JS/instanceof/index.ts
@@ -1,10 +1,10 @@
 /* 实现 instanceof 操作符 */
 export function instanceOf(obj: Record<string, any>, constructor: Record<string, any>) {
-  const prototype = constructor.prototype;
+  const prototype = constructor.prototype; // 挂在构造函数上的原型
   let current = obj;
   while (current /* 原型链的根为 null，所以能跳出循环 */) {
     if (current === prototype) return true;
-    else current = Object.getPrototypeOf(current);
+    else current = Object.getPrototypeOf(current); // 实例的指向的原型
   }
   return false;
 }
