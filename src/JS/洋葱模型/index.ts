ac07ed1be1f8aafb4f3453e09af0b1da5c1da3f0
Shenhaoyi <shenhaoyi04@163.com>
2024-01-05 19:50:22 +0800

    优化代码;补充注释

--- src/JS/洋葱模型/index.ts
+++ src/JS/洋葱模型/index.ts
@@ -1,4 +1,4 @@
-/* 
+/*
   任务执行的洋葱模型：调用next可以先去执行下一个任务，如果没有调用，则自动在当前任务结束后调用
   参考：https://v.douyin.com/idFNQws5/
 */
@@ -13,27 +13,30 @@ export class TaskPro {
   }
   async next() {
     this.currentIndex++;
-    await this.runOneTask();
+    await this.innerRun();
   }
   reset() {
     this.taskList = [];
     this.isRunning = false;
     this.currentIndex = 0;
   }
-  private async runOneTask() {
+  // 内部调用，执行一个任务
+  private async innerRun() {
     if (this.currentIndex > this.taskList.length - 1) return; // 边界条件
     const task = this.taskList[this.currentIndex];
     const tempIndex = this.currentIndex;
     await task(this.context, this.next.bind(this)); // this指向问题！！
     if (this.currentIndex === tempIndex) {
+      // 如果 task 内部没有调用 next，则需要在这里调用
       await this.next();
     }
   }
+  // 外部调用，开始执行任务
   async run() {
     if (this.isRunning || this.taskList.length === 0) return; // 在run或者无task可run
     this.isRunning = true; //
-    this.context = {};
-    await this.runOneTask();
+    this.context = {}; // context 在这里重置
+    await this.innerRun();
     this.reset();
   }
 }
