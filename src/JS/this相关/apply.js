12432d2bd0a9ce13fcf9ae8480cfd36ed73339e6
Shenhaoyi <shenhaoyi04@163.com>
2024-04-26 16:10:58 +0800

    fix

--- src/JS/this相关/apply.js
+++ src/JS/this相关/apply.js
@@ -4,7 +4,7 @@
 Function.prototype.myApply = function (target, args) {
   const _this = this; // x.mycall, 所以this就是x，即要绑定到target上的函数
   const name = _this.name; // 可以拿到函数的名字
-  Reflect.defineProperty(target, name, { value: _this }); // 等价于 target[name] = _this;
+  Reflect.set(target, name, _this); // 等价于 target[name] = _this;
   const result = target[name](...args);
   Reflect.deleteProperty(target, name);
   return result;
