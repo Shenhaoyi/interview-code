821a026025133375dad44a404d9afe39e9695d41
Shenhaoyi <shenhaoyi04@163.com>
2024-04-26 16:09:22 +0800

    优化代码

--- src/JS/this相关/call.js
+++ src/JS/this相关/call.js
@@ -4,8 +4,8 @@
 Function.prototype.myCall = function (target, ...args) {
   const _this = this; // x.mycall, 所以this就是x，即要绑定到target上的函数
   const name = _this.name; // 可以拿到函数的名字
-  Reflect.defineProperty(target, name, { value: _this }); // 等价于 target[name] = _this;
-  const result = target[name](...args); // 记得要讲过返回！！
+  Reflect.set(target, name, _this); // 等价于 target[name] = _this;
+  const result = target[name](...args); // 记得要将结果过返回！！
   Reflect.deleteProperty(target, name);
   return result;
 };
