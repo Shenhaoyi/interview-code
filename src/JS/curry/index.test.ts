5d5e95bf06b76ffd684466ecdfa16532c22e5647
Shenhaoyi <shenhaoyi04@163.com>
2024-05-05 15:39:03 +0800

    删除无用代码

--- src/JS/curry/index.test.ts
+++ src/JS/curry/index.test.ts
@@ -3,8 +3,6 @@ test('curry', () => {
   function add(a: number, b: number, c: number, d: number) {
     return a + b + c + d;
   }
-  console.log(curry(add)(1, 2)(3, 4));
-
   expect(curry(add)(1, 2, 3, 4)).toBe(10);
   expect(curry(add)(1, 2)(3, 4)).toBe(10);
   expect(curry(add)(1)(2)(3, 4)).toBe(10);
